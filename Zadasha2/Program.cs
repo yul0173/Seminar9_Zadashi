84185f9134e4f18fafd88ff741a1366a24575bf9
Юлия <108525379+yul0173@users.noreply.github.com>
2022-08-20 11:12:08 +0300

    Задача 2

---
+++ Zadasha2/Program.cs
@@ -0,0 +1,10 @@
+﻿// Задайте значения M и N. Напишите программу, которая найдёт сумму натуральных элементов в промежутке от M до N
+int n, m, s=0;
+Console.Write("Введи значение m: ");
+n = Convert.ToInt16(Console.ReadLine());
+Console.Write("Введи значение n: ");
+m = Convert.ToInt16(Console.ReadLine());
+for (int i = n; i <= m; i++)
+    s += i;
+Console.WriteLine(s);
+
