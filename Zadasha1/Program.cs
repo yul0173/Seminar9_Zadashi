77e053cf317ee3fb7ba0dfad335411b1179d9e78
Юлия <108525379+yul0173@users.noreply.github.com>
2022-08-20 11:09:27 +0300

    Задача 1

---
+++ Zadasha1/Program.cs
@@ -0,0 +1,11 @@
+﻿// Задайте значения M и N. Напишите программу, которая выведет все натуральные числа в промежутке от M до N.
+Console.Write("Введи M: ");
+int M = Convert.ToInt16(Console.ReadLine());
+Console.Write("Введи N: ");
+int N = Convert.ToInt16(Console.ReadLine());
+if (M>N)
+for (int i = N; i <= M; i++)
+Console.Write($" {i}");
+else
+    for (int i = M; i <= N; i++)
+Console.Write($" {i}");
